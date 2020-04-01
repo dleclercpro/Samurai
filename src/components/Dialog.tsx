9b41b7145f50e6dddc94757a8915b7f1d4efb989
David Leclerc <d.leclerc.pro@gmail.com>
2020-04-01 19:46:41 -0400

    No click propagation in dialogs.

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -16,17 +16,22 @@ interface DialogProps {
 
 class Dialog extends React.Component<DialogProps, {}> {
 
+    handleClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
+    }
+
     render() {
         const { children, headline, closeDialog } = this.props;
 
         return (
-            <div className='dialog'>
+            <div className='dialog' onClick={this.handleClick}>
                 <h2 className='headline'>{headline}</h2>
                 <div className='content'>
                     {children}
                 </div>
                 <div className='buttons'>
-                    <Button action={() => {}}>Click me!</Button>
+                    <Button action={closeDialog}>Cancel</Button>
+                    <Button action={closeDialog}>OK</Button>
                 </div>
                 <CloseIcon className='icon-close' onClick={closeDialog} />
             </div>
