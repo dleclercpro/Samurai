4ae648952841766a0b3da458d71c636111fa7da5
David Leclerc <d.leclerc.pro@gmail.com>
2020-05-14 23:26:24 -0400

    Remember locations when redirecting.

--- src/redirect.ts
+++ src/redirect.ts
@@ -1,17 +1,17 @@
 export const redirectHome = (): Promise<void> => {
-    document.location.replace('/');
+    document.location.assign('/');
 
     return Promise.resolve();
 }
 
 export const redirectRules = (): Promise<void> => {
-    document.location.replace('/rules/');
+    document.location.assign('/rules/');
 
     return Promise.resolve();
 }
 
 export const redirectGame = (id: number): Promise<void> => {
-    document.location.replace(`/game/${id}/`);
+    document.location.assign(`/game/${id}/`);
 
     return Promise.resolve();
 }
