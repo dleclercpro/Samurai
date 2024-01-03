0ec7ede6a9c042761b976235bf7501daef7c54ca
David Leclerc <d.leclerc.pro@gmail.com>
2024-01-03 13:35:15 -0500

    Feat: use init conf for NGINX, then reload using final conf, whereby HTTPS is handled.

--- Apps/ReverseProxy/entrypoint.sh
+++ Apps/ReverseProxy/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Paths
+PATH_NGINX_INIT_CONF="/etc/nginx/init.conf"
+PATH_NGINX_FINAL_CONF="/etc/nginx/nginx.conf"
+PATH_CERTBOT_WEBROOT="/var/www/html"
+
+# Deploy hook used by Certbot: ensures NGINX is reloaded using
+# the final configuration (w/ HTTPS)
+DEPLOY_HOOK="nginx -s reload -c $PATH_NGINX_FINAL_CONF"
+
 # Function to renew certificates
 renew_ssl() {
     certbot renew
@@ -7,14 +16,23 @@ renew_ssl() {
 
 # Function to obtain an initial SSL certificate
 init_ssl() {
-    certbot certonly --webroot --webroot-path=/var/www/html -d $DOMAIN --email $EMAIL --agree-tos
+    certbot certonly \
+        --non-interactive --agree-tos \
+        --webroot --webroot-path=$PATH_CERTBOT_WEBROOT \
+        -d $DOMAIN --email $EMAIL \
+        --deploy-hook $DEPLOY_HOOK
 }
+
+# Print out environment variables as specified in Docker Compose
+# file (they should be accessible)
+echo "Domain: $DOMAIN"
+echo "Email: $EMAIL"
 
 # Get first SSL certificate
 init_ssl
 
-# Start NGINX
-nginx -g 'daemon off;' &
+# Start NGINX with the initial configuration (w/o HTTPS)
+nginx -g "daemon off;" -c $PATH_NGINX_INIT_CONF &
 
 # Schedule SSL certificate renewal every day
 while :; do
