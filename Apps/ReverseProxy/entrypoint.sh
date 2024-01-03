bf4996b0923547d24e2366ece384f8cffff2c727
David Leclerc <d.leclerc.pro@gmail.com>
2024-01-03 13:42:19 -0500

    Fix: ambiguous variable names.

--- Apps/ReverseProxy/entrypoint.sh
+++ Apps/ReverseProxy/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Paths
-PATH_NGINX_INIT_CONF="/etc/nginx/init.conf"
-PATH_NGINX_FINAL_CONF="/etc/nginx/nginx.conf"
-PATH_CERTBOT_WEBROOT="/var/www/html"
+NGINX_INIT_CONF="/etc/nginx/init.conf"
+NGINX_FINAL_CONF="/etc/nginx/nginx.conf"
+CERTBOT_WEBROOT="/var/www/html"
 
 # Deploy hook used by Certbot: ensures NGINX is reloaded using
 # the final configuration (w/ HTTPS)
-DEPLOY_HOOK="nginx -s reload -c $PATH_NGINX_FINAL_CONF"
+DEPLOY_HOOK="nginx -s reload -c $NGINX_FINAL_CONF"
 
 # Function to renew certificates
 renew_ssl() {
@@ -18,9 +18,9 @@ renew_ssl() {
 init_ssl() {
     certbot certonly \
         --non-interactive --agree-tos \
-        --webroot --webroot-path=$PATH_CERTBOT_WEBROOT \
-        -d $DOMAIN --email $EMAIL \
-        --deploy-hook $DEPLOY_HOOK
+        --webroot --webroot-path="$CERTBOT_WEBROOT" \
+        -d "$DOMAIN" --email "$EMAIL" \
+        --deploy-hook "$DEPLOY_HOOK"
 }
 
 # Print out environment variables as specified in Docker Compose
@@ -32,7 +32,7 @@ echo "Email: $EMAIL"
 init_ssl
 
 # Start NGINX with the initial configuration (w/o HTTPS)
-nginx -g "daemon off;" -c $PATH_NGINX_INIT_CONF &
+nginx -g "daemon off;" -c $NGINX_INIT_CONF &
 
 # Schedule SSL certificate renewal every day
 while :; do
