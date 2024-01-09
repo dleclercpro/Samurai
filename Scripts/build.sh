7d9041e7aa31eaeac5096aed110d2aebf7c3b521
David Leclerc <d.leclerc.pro@gmail.com>
2024-01-09 13:27:20 -0500

    Release tag.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -6,7 +6,7 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Define image variables
 user="dleclercpro"
 app="samurai"
-release="latest"
+release="2.6.0"
 
 # Build a cross-platform image and push it to Dockerhub
 docker buildx build --platform linux/amd64,linux/arm64 -t $user/$app:$release -f ./Dockerfile . --push
